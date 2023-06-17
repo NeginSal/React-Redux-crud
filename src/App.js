f69093d90778f628d32f29d3f8b47c57ea07697a
NeginSal <neginsaljooghi@gmail.com>
2023-06-17 17:07:54 +0330

    added component and dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import EditUser from './components/EditUser';
+import AddUser from './components/AddUser';
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-     
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/edit/:id' element={<EditUser />} />
+        <Route path='/create' element={< AddUser />} />
+      </Routes>
+    </>
   );
 }
 
