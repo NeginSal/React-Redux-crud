c56ba491114869491efb78cade7ca3b268fa3356
NeginSal <neginsaljooghi@gmail.com>
2023-06-18 17:26:35 +0330

    added provider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import EditUser from './components/EditUser';
 import AddUser from './components/AddUser';
+import { Provider } from 'react-redux';
+import Store from './Redux/Store';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/edit/:id' element={<EditUser />} />
-        <Route path='/create' element={< AddUser />} />
-      </Routes>
+      <Provider store={Store}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/edit/:id' element={<EditUser />} />
+          <Route path='/create' element={< AddUser />} />
+        </Routes>
+      </Provider>
     </>
   );
 }
